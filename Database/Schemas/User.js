363d99a1b22cac69c93bd0973f09a47a605bd156
saulikarvinen <saulikarvinen@outlook.com>
2022-02-18 10:24:17 +0200

    Updated user schema

--- Database/Schemas/User.js
+++ Database/Schemas/User.js
@@ -1,9 +1,35 @@
 const mongoose = rewuire("mongoose");
 
 const User = mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  name: String,
+  profileimg: Buffer,
+  profilebackgroundimage: Buffer,
+  email: String,
+  title: String,
+  organization: String,
+  posts: [String],
+  bio: String,
+  contacts: [String],
+  github: String,
+  workhistory: [String],
+  messages: [String],
+  password: String,
   refreshTokens: [String],
 });
+
+/* 
+this.name = name;
+    this.profileimg = profileimg;
+    this.profilebackgroundimage = profilebackgroundimage;
+    this.email = email;
+    this.title = title;
+    this.organization = organization;
+    this.posts = [];
+    this.bio = "";
+    this.contacts = [];
+    this.github = "";
+    this.workhistory = [];
+    this.messages = [];
+*/
 
 module.exports = mongoose.model("userschema", User);
--- Routes/signup.js
+++ Routes/signup.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
+router.post("/", (req, res) => {
+  jwt.sign(user, process.env.ACCESS_TOKEN, {
+    expiresIn: 15000,
+  });
+});
+
 module.exports = router;
